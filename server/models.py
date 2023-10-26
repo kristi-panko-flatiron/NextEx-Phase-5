a6a4202fd40ce2a00677f6429fb7959df08cedd9
kris-panko <krispanko@gmail.com>
2023-10-26 19:10:02 -0400

    model tables working and seed data

--- server/models.py
+++ server/models.py
@@ -1,6 +1,87 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import validates
+from datetime import datetime
 from config import db
 
-# Models go here!
+# User model
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    birthday = db.Column(db.String(50), nullable=False)
+    astrological_sign_id = db.Column(db.Integer, db.ForeignKey('astrological_signs.id'))
+    astrological_sign = relationship('AstrologicalSign', back_populates='users')
+    matches = db.relationship('Match', secondary='user_matches', back_populates='users')
+    serialize_rules = ('-user_matches.user',)
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name must be provided.")
+        return name
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be provided.")
+        return username
+
+    @validates('password')
+    def validate_password(self, key, password):
+        if not password:
+            raise ValueError("Password must be provided.")
+        return password
+
+    @validates('astrological_sign_id')
+    def validate_astrological_sign_id(self, key, astrological_sign_id):
+        if not astrological_sign_id:
+            raise ValueError("Valid astrological sign must be provided.")
+        return astrological_sign_id
+    
+    @validates('birthday')
+    def validate_birthday(self, key, birthday):
+        try:
+            
+            date_format = "%Y-%m-%d"
+            datetime.strptime(birthday, date_format)
+            dob = datetime.strptime(birthday, date_format)
+            today = datetime.today()
+            age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+            if not 18 <= age <= 99:
+                raise ValueError("Age must be between 18 and 99.")
+            return birthday
+        except ValueError:
+            raise ValueError("Invalid date format. Please use 'YYYY-MM-DD'.")
+
+# UserMatch model (intermediary table)
+class UserMatch(db.Model):
+    __tablename__ = 'user_matches'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'), primary_key=True)
+    user = relationship('User', back_populates='matches')
+    match = relationship('Match', back_populates='users')
+
+# Match model
+class Match(db.Model, SerializerMixin):
+    __tablename__ = 'matches'
+    id = db.Column(db.Integer, primary_key=True)
+    match_date = db.Column(db.Date)
+    users = db.relationship('User', secondary='user_matches', back_populates='matches')
+    serialize_rules = ('-user_matches.user', '-match.user_matches',)
+
+# AstrologicalSign model
+class AstrologicalSign(db.Model, SerializerMixin):
+    __tablename__ = 'astrological_signs'
+    id = db.Column(db.Integer, primary_key=True)
+    sign_name = db.Column(db.String(20), nullable=False)
+    sign_description = db.Column(db.String(200))
+    best_matches = db.relationship('BestMatch', back_populates='astrological_sign')
+
+# BestMatch model
+class BestMatch(db.Model):
+    __tablename__ = 'best_matches'
+    id = db.Column(db.Integer, primary_key=True)
+    astrological_sign_id = db.Column(db.Integer, db.ForeignKey('astrological_signs.id'))
+    best_match_name = db.Column(db.String(20), nullable=False)
+    astrological_sign = relationship('AstrologicalSign', back_populates='best_matches')
--- server/seed.py
+++ server/seed.py
@@ -9,9 +9,94 @@ from faker import Faker
 # Local imports
 from app import app
 from models import db
+from app import db, User, AstrologicalSign, Match, BestMatch
+# Create users
+users_data = [
+    {'name': 'Kris', 'birthday': '1990-05-15', 'username': 'kris123', 'password': 'password123', 'astrological_sign_id': 1},
+    {'name': 'Idalis', 'birthday': '1990-05-15', 'username': 'idalis456', 'password': 'password456', 'astrological_sign_id': 3},
+    {'name': 'Tano', 'birthday': '1990-05-15', 'username': 'tano789', 'password': 'password789', 'astrological_sign_id': 2},
+    {'name': 'Matthew', 'birthday': '1990-05-15', 'username': 'matthew987', 'password': 'password987', 'astrological_sign_id': 4},
+    # Add more users...
+]
+
+for user_data in users_data:
+    user = User(**user_data)
+    db.session.add(user)
+
+# Create astrological signs
+signs_data = [
+    {'sign_name': 'Aries', 'sign_description': 'Direct, passionate, and headstrong.'},
+    {'sign_name': 'Taurus', 'sign_description': 'Practical, pragmatic, steadfast, dedicated, dependable, and stubborn.'},
+    {'sign_name': 'Gemini', 'sign_description': 'Chatty, amicable, and easy to get along with! A zesty personality, they may be somewhat of an entertainer but also flaky and indecisive. '},
+    {'sign_name': 'Cancer', 'sign_description': 'Loyal, protective, and intuitive but may become too much creatures of comfort stuck in their routines.'},
+    {'sign_name': 'Leo', 'sign_description': 'Confident, extravagant (in gestures and money), loyal, and ambitious.'},
+    {'sign_name': 'Virgo', 'sign_description': 'Practical, grounded, detail-oriented, meticulous, as well as flexible and open to change'},
+    {'sign_name': 'Libra', 'sign_description': 'A strong sense of fairness and justice, and are are eager mediators. They are idealistic and imaginative, but may be indecisive and self-pitying.'},
+    {'sign_name': 'Scorpio', 'sign_description': 'A keen sense of perception but may keep themselves hidden, prone to secrecy. They are determined, ambitious, and obsessive.'},
+    {'sign_name': 'Sagittarius', 'sign_description': 'Spiritual, intellectual, and sarcastic. Spontaneous lovers of freedom who love to converse with a wide array of people.'},
+    {'sign_name': 'Capricorn', 'sign_description': 'Resilient, persistent, realistic, and disciplined. They may place an importance on career, social status or money, and have a strong work ethic. They are capable leaders and loyal team players but may be prone to stubbornness.'},
+    {'sign_name': 'Aquarius', 'sign_description': 'Progressive, independent, and idealistic. They’re comfortable on the fringes of society and have a unique vision of the world.'},
+    {'sign_name': 'Pisces', 'sign_description': 'Highly creative, as well as being friendly, sensitive, kind, and caring.'}
+]
+
+for sign_data in signs_data:
+    sign = AstrologicalSign(**sign_data)
+    db.session.add(sign)
+
+# Create matches
+matches_data = [
+    {'match_date': '2023-10-24'},
+    {'match_date': '2023-10-22'},
+    # Add more matches...
+]
+
+for match_data in matches_data:
+    match = Match(**match_data)
+    db.session.add(match)
+
+# Create best matches
+best_matches_data = [
+    {'astrological_sign_id': 1, 'best_match_name': 'Leo'},
+    {'astrological_sign_id': 1, 'best_match_name': 'Sagittarius'},
+    {'astrological_sign_id': 2, 'best_match_name': 'Virgo'},
+    {'astrological_sign_id': 2, 'best_match_name': 'Capricorn'},
+    {'astrological_sign_id': 3, 'best_match_name': 'Libra'},
+    {'astrological_sign_id': 3, 'best_match_name': 'Aquarius'},
+    {'astrological_sign_id': 4, 'best_match_name': 'Scorpio'},
+    {'astrological_sign_id': 4, 'best_match_name': 'Pisces'},
+    {'astrological_sign_id': 5, 'best_match_name': 'Aries'},
+    {'astrological_sign_id': 5, 'best_match_name': 'Sagittarius'},
+    {'astrological_sign_id': 6, 'best_match_name': 'Taurus'},
+    {'astrological_sign_id': 6, 'best_match_name': 'Capricorn'},
+    {'astrological_sign_id': 7, 'best_match_name': 'Gemini'},
+    {'astrological_sign_id': 7, 'best_match_name': 'Aquarius'},
+    {'astrological_sign_id': 8, 'best_match_name': 'Cancer'},
+    {'astrological_sign_id': 8, 'best_match_name': 'Pisces'},
+    {'astrological_sign_id': 9, 'best_match_name': 'Aries'},
+    {'astrological_sign_id': 9, 'best_match_name': 'Leo'},
+    {'astrological_sign_id': 10, 'best_match_name': 'Scorpio'},
+    {'astrological_sign_id': 10, 'best_match_name': 'Pisces'},
+    {'astrological_sign_id': 11, 'best_match_name': 'Gemini'},
+    {'astrological_sign_id': 11, 'best_match_name': 'Libra'},
+    {'astrological_sign_id': 12, 'best_match_name': 'Cancer'},
+    {'astrological_sign_id': 12, 'best_match_name': 'Scorpio'}
+    # Add more best matches...
+]
+
+for best_match_data in best_matches_data:
+    best_match = BestMatch(**best_match_data)
+    db.session.add(best_match)
+
+# Commit the session to the database
+db.session.commit()
+
+print("Seed data added successfully.")
+
+
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        
+
