e15aab6424d8279c7b05b3d5d31956d935a97845
kris-panko <krispanko@gmail.com>
2023-10-27 13:32:10 -0400

    added serialize rules

--- server/models.py
+++ server/models.py
@@ -12,44 +12,45 @@ class User(db.Model):
     astrological_sign_id = Column(Integer, ForeignKey('astrological_signs.id'))
     astrological_sign = relationship("AstrologicalSign", back_populates="users")
     matches = relationship("UserMatch", back_populates="user")
+    serialize_rules = ('-user_matches.user',)
 
-    # @validates('name')
-    # def validate_name(self, key, name):
-    #     if not name:
-    #         raise ValueError("Name must be provided.")
-    #     return name
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name must be provided.")
+        return name
 
-    # @validates('username')
-    # def validate_username(self, key, username):
-    #     if not username:
-    #         raise ValueError("Username must be provided.")
-    #     return username
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be provided.")
+        return username
 
-    # @validates('password')
-    # def validate_password(self, key, password):
-    #     if not password:
-    #         raise ValueError("Password must be provided.")
-    #     return password
+    @validates('password')
+    def validate_password(self, key, password):
+        if not password:
+            raise ValueError("Password must be provided.")
+        return password
 
-    # @validates('astrological_sign_id')
-    # def validate_astrological_sign_id(self, key, astrological_sign_id):
-    #     if not astrological_sign_id:
-    #         raise ValueError("Valid astrological sign must be provided.")
-    #     return astrological_sign_id
+    @validates('astrological_sign_id')
+    def validate_astrological_sign_id(self, key, astrological_sign_id):
+        if not astrological_sign_id:
+            raise ValueError("Valid astrological sign must be provided.")
+        return astrological_sign_id
     
-    # @validates('birthday')
-    # def validate_birthday(self, key, birthday):
-    #     try:
-    #         date_format = "%Y-%m-%d"
-    #         datetime.strptime(birthday, date_format)
-    #         dob = datetime.strptime(birthday, date_format)
-    #         today = datetime.today()
-    #         age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
-    #         if not 18 <= age <= 99:
-    #             raise ValueError("Age must be between 18 and 99.")
-    #         return birthday
-    #     except ValueError:
-    #         raise ValueError("Invalid date format. Please use 'YYYY-MM-DD'.")
+    @validates('birthday')
+    def validate_birthday(self, key, birthday):
+        try:
+            date_format = "%Y-%m-%d"
+            datetime.strptime(birthday, date_format)
+            dob = datetime.strptime(birthday, date_format)
+            today = datetime.today()
+            age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+            if not 18 <= age <= 99:
+                raise ValueError("Age must be between 18 and 99.")
+            return birthday
+        except ValueError:
+            raise ValueError("Invalid date format. Please use 'YYYY-MM-DD'.")
 
 
 class AstrologicalSign(db.Model):
@@ -59,6 +60,7 @@ class AstrologicalSign(db.Model):
     sign_description = Column(String(200), nullable=True)
     users = relationship("User", back_populates="astrological_sign")
     best_matches = relationship("BestMatch", back_populates="astrological_sign")
+    serialize_rules = ('-users.astrological_sign', '-best_matches.astrological_sign',)
 
 class UserMatch(db.Model):
     __tablename__ = 'user_matches'
@@ -66,12 +68,14 @@ class UserMatch(db.Model):
     match_id = Column(Integer, ForeignKey('matches.id'), primary_key=True)
     user = relationship("User", back_populates="matches")
     match = relationship("Match", back_populates="users")
+    serialize_rules = ('-user.matches', '-match.users',)
 
 class Match(db.Model):
     __tablename__ = 'matches'
     id = Column(Integer, primary_key=True)
     match_date = Column(Date, nullable=True)
     users = relationship("UserMatch", back_populates="match")
+    serialize_rules = ('-users.match',)
 
 class BestMatch(db.Model):
     __tablename__ = 'best_matches'
@@ -79,3 +83,4 @@ class BestMatch(db.Model):
     astrological_sign_id = Column(Integer, ForeignKey('astrological_signs.id'))
     best_match_name = Column(String(20), nullable=False)
     astrological_sign = relationship("AstrologicalSign", back_populates="best_matches")
+    serialize_rules = ('-astrological_sign.best_matches',)
