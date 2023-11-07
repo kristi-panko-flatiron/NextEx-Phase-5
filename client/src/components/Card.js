9c6bb0d15228d5a4c873f4e6d6517b8df0e949c4
kris-panko <krispanko@gmail.com>
2023-11-07 03:15:39 -0500

    more styling

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,17 +4,22 @@ import { useHistory } from 'react-router-dom';
 
 const Card = ({ user, onAddToFavorites }) => {
     const history = useHistory();
+    const placeholderImage = 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-514622028-e1617288074638.jpg'; 
 
     // Function to handle the navigation
     const navigateToUserDetail = () => {
         history.push(`/users/${user.id}`);
     };
 
+    // Determine which image to use
+    const imageUrl = user.image_url || placeholderImage;
+
     return (
         <div className="card" onClick={navigateToUserDetail}>
             <div className="card-content">
                 <h2>{user.name}</h2>
-                <img src={user.image_url} alt={user.name} />
+                {/* Use the imageUrl which could be the user's image or the placeholder */}
+                <img src={imageUrl} alt={user.name} />
                 <p>Astrological Sign: {user.astrological_sign ? user.astrological_sign.sign_name : 'Loading...'}</p>
                 <button onClick={(e) => {
                     e.stopPropagation(); 
--- client/src/components/FavoritesBar.js
+++ client/src/components/FavoritesBar.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../index.css'
-
+import React from 'react';
+import '../index.css';
 
 const FavoritesBar = ({ favorites, removeFromFavorites }) => {
+    const placeholderImage = 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-514622028-e1617288074638.jpg';
+
     return (
         <aside className="favorites-bar">
             <h3 className="gradient-text">Favorites</h3>
             <div className="favorite-list">
                 {favorites.map((favorite) => (
                     <div key={favorite.id} className="favorite-item">
-                        <img src={favorite.image_url} alt={favorite.name} className="favorite-image" />
+                        {/* Use either the favorite's image or the placeholder image */}
+                        <img src={favorite.image_url || placeholderImage} alt={favorite.name} className="favorite-image" />
                         <p>{favorite.name}</p>
                         <p>{favorite.astrologicalSign}</p>
                         <button onClick={() => removeFromFavorites(favorite)}>💔</button>
@@ -21,4 +22,4 @@ const FavoritesBar = ({ favorites, removeFromFavorites }) => {
     );
 };
 
-export default FavoritesBar;// HomePage.js
+export default FavoritesBar;
